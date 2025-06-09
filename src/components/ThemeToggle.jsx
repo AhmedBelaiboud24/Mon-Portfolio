e525614bc1b39583d3eff1598c1beca38462bd71
Ahmed Belaiboud <ahmedbelaiboud21@gmail.com>
2025-06-09 18:13:23 +0100

    d ahecc

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,34 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import Switch from '@mui/material/Switch';
-import { LightMode, DarkMode } from '@mui/icons-material'; 
+import React from 'react';
 
 const ThemeToggle = () => {
-    const [checked, setChecked] = useState(false);
-
-    useEffect(() => {
-        const savedTheme = localStorage.getItem('theme') || 'dark';
-        document.documentElement.setAttribute('data-theme', savedTheme);
-        setChecked(savedTheme === 'light'); 
-    }, []);
-
-    const handleChange = (event) => {
-        const newTheme = event.target.checked ? 'light' : 'dark';
-        document.documentElement.setAttribute('data-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
-        setChecked(event.target.checked);
-    };
-
-    return (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-            <DarkMode style={{ marginRight: '8px' }} /> {}
-            <Switch
-                checked={checked}
-                onChange={handleChange}
-                inputProps={{ 'aria-label': 'controlled' }}
-            />
-            <LightMode style={{ marginLeft: '8px' }} /> {}
-        </div>
-    );
+    return null;
 };
 
 export default ThemeToggle;
